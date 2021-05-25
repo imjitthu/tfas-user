666d963bc35a740972564ebec4b30b08fca4b56e
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-25 13:20:04 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -36,6 +36,6 @@ resource "aws_route53_record" "jithendar" {
   name          = "${var.COMPONENT}.${data.aws_route53_zone.jithendar.name}"
   type          = "A"
   ttl           = "300"
-  zone_id       = "data.aws_route53_zone.jithendar.zone_id"
+  zone_id       = data.aws_route53_zone.jithendar.zone_id
   records       = [aws_instance.user.public_ip]
 }
--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
 provider "aws" {
-  profile = "terraform"
   region  = "us-east-1"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.21"
-    }
-  }
 }
