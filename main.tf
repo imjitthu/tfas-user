9719ca4d6d08ecc85257971e4d72e0c7ce032717
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-25 14:16:41 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "user" {
   # aws_spot_instabce_request for spot instance
   ami = data.aws_ami.ami.id
   instance_type = "${var.INSTANCE_TYPE}"
-  key_name = local.key_name
+  #key_name = local.key_name
   # spot_type = "one-time"  aws_spot_instance_request
   tags = {
     "Name" = "${var.COMPONENT}-Server"
