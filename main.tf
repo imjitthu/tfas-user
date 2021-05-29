723915fbfd27d7777dcc7b50c10fc3d47cf73053
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-29 13:05:47 +0530

    rs-webapp

--- main.tf
+++ main.tf
@@ -4,19 +4,15 @@ locals {
 }
 
 resource "aws_instance" "user" {
-  # aws_spot_instabce_request for spot instance
   ami = data.aws_ami.ami.id
   instance_type = "${var.INSTANCE_TYPE}"
-  #key_name = local.key_name
-  # spot_type = "one-time"  aws_spot_instance_request
   tags = {
-    "Name" = "${var.COMPONENT}-Server"
+    "Name" = "${var.COMPONENT}-${var.env}"
   }
 
 connection {
   host = aws_instance.user.public_ip
   type = "ssh"
-  #private_key = file("${local.key_path}") read private_key (PEM) from file
   user = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["SSH_USER"]
   password = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["SSH_PASS"]
 }
@@ -27,8 +23,6 @@ provisioner "remote-exec" {
 
 provisioner "local-exec" {
   command = "echo ${aws_instance.user.public_ip} > user_inv"
-  #command = "ansible-playbook -i ${aws_instance.user.public_ip}, --private-key ${local.key_path} ${var.COMPONENT}.yml"
-  #echo $IP component=${component} ansible_user=root ansible_password=DevOps321 >>inv
 }
 }
 
